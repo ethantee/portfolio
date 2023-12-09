62b40debf62260cb015db30282db22720a5f0a3f
Ethan Tee <108862202+ethantee@users.noreply.github.com>
2023-12-09 10:39:54 +0800

    update code

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer className="w-full border-t border-[#33353F]">
+            <div className="mb-14 w-full container mx-auto p-4 flex justify-between items-center text-slate-400">
+                <span className="">Kai Yau</span>
+                <p>Copyright &copy; All rights reserved</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
